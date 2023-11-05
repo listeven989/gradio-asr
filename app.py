066698aa5e406a50c420a728996b8b12f59b8cb4
steven4354 <steven4354@gmail.com>
2023-11-04 21:38:59 -0600

    whisper

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
 import gradio as gr
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
-import torch
+from transformers import pipeline
 import numpy as np
-import librosa
 
-# Load pre-trained model and processor
-processor = Wav2Vec2Processor.from_pretrained("openai/whisper-base.en")
-model = Wav2Vec2ForCTC.from_pretrained("openai/whisper-base.en")
+transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
 
 def transcribe(stream, new_chunk):
     sr, y = new_chunk
     y = y.astype(np.float32)
-    y /= np.max(np.abs(y))
-
-    # Resample the audio to 16kHz
-    y = librosa.resample(y, orig_sr=sr, target_sr=16000)
-    sr = 16000
+    if np.max(np.abs(y)) != 0:
+        y /= np.max(np.abs(y))
 
     if stream is not None:
         stream = np.concatenate([stream, y])
     else:
         stream = y
+    return stream, transcriber({"sampling_rate": sr, "raw": stream})["text"]
 
-    input_values = processor(stream, sampling_rate=sr, return_tensors="pt").input_values
-    logits = model(input_values).logits
-    predicted_ids = torch.argmax(logits, dim=-1)
-    transcription = processor.decode(predicted_ids[0])
-
-    return stream, transcription
 
 demo = gr.Interface(
     transcribe,
